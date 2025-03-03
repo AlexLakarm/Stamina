1cc77121e87947b9f11e56ffa9f3dbe1cbd95019
alexandrekermarec <alexkermarec@hotmail.fr>
2025-03-03 09:05:42 +0100

    removed gg analytics and cookiebot

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -9,12 +9,11 @@ import Script from 'next/script'
 
 const inter = Inter({ subsets: ["latin"] });
 
-const GA_MEASUREMENT_ID = 'G-SYRS9R9QRG';
 const GTM_ID = 'GTM-T2Z3K44V';
 
 export const metadata: Metadata = {
   title: "Stamina - Dev & Consulting",
-  description: "Stamina est une entreprise de développement et de conseil en blockchain. Nous vous accompagnons dans la transition vers le Web3.",
+  description: "Expert blockchain, Stamina vous accompagne dans vos projets Web3 avec des solutions sur mesure en développement et conseil.",
 };
 
 export default function RootLayout({
@@ -25,48 +24,28 @@ export default function RootLayout({
   return (
     <html lang="fr" suppressHydrationWarning className="h-full">
       <head>
+        {/* Google Tag Manager */}
         <Script
-          id="Cookiebot"
-          src="https://consent.cookiebot.com/uc.js"
-          data-cbid="55657b3e-2d5a-4e19-83b4-53eed0cbd743"
-          data-blockingmode="auto"
+          id="gtm"
           strategy="beforeInteractive"
-          async={true}
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');`
+          }}
         />
-        
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-          strategy="afterInteractive"
-        />
-        <Script id="cookie-dependent-scripts" strategy="afterInteractive">
-          {`
-            window.addEventListener('CookiebotOnAccept', function (e) {
-              if (Cookiebot.consent.statistics) {
-                // Google Analytics
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${GA_MEASUREMENT_ID}');
-
-                // Google Tag Manager
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','${GTM_ID}');
-              }
-            });
-          `}
-        </Script>
       </head>
       <body className={`${inter.className} min-h-screen flex flex-col`}>
+        {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe 
             src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-            height="0" 
-            width="0" 
+            height="0"
+            width="0"
             style={{ display: 'none', visibility: 'hidden' }}
-          />
+          ></iframe>
         </noscript>
 
         <ThemeProvider
--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,12 +3,6 @@ const nextConfig = {
     output: 'export',
     images: {
         unoptimized: true,
-    },
-    env: {
-        EMAIL_HOST: process.env.EMAIL_HOST,
-        EMAIL_PORT: process.env.EMAIL_PORT,
-        EMAIL_USER: process.env.EMAIL_USER,
-        EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,
     }
 }
 
